42f2ec3cc42a7ce1b889f439ffe5ffac43ee3c0b
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-05-04 23:25:39 +0300

    recipes row layout and placeholder

--- app/src/main/java/com/example/foodservice2/RecipesFragment.kt
+++ app/src/main/java/com/example/foodservice2/RecipesFragment.kt
@@ -5,15 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foodservice2.databinding.FragmentRecipesBinding
+import com.facebook.shimmer.ShimmerFrameLayout
 
 class RecipesFragment : Fragment() {
+
+
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.shimmerFrameLayout.startShimmer()
+
+
+
+
+
+
+        return view
     }
 
 }
+
+
