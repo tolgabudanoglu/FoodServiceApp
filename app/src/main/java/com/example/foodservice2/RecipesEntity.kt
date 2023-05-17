d43012a2e7c8567fbc1164312e2890b18f87a19d
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-05-17 18:24:01 +0300

    create recipes entity and dao

---
+++ app/src/main/java/com/example/foodservice2/RecipesEntity.kt
@@ -0,0 +1,15 @@
+package com.example.foodservice2
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.foodservice2.util.Constants.Companion.RECIPES_TABLE
+import com.example.foodservice2.models.FoodRecipes
+
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipes
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
