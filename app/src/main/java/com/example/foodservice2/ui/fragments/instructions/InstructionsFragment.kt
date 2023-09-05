8207d652b195bddbd78771edce92202260689749
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-09-05 18:29:59 +0300

    instructions fragment finished

--- app/src/main/java/com/example/foodservice2/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foodservice2/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,19 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.foodservice2.R
+import com.example.foodservice2.databinding.FragmentInstructionsBinding
+import com.example.foodservice2.models.Result
+import com.example.foodservice2.util.Constants
+import com.example.foodservice2.util.retrieveParcelable
 
 
 class InstructionsFragment : Fragment() {
 
 
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args!!.retrieveParcelable(Constants.RECIPE_RESULT_KEY) as Result?
+
+        if (myBundle != null) {
+            binding.wvInstructions.webViewClient = object : WebViewClient() {}
+            val websiteUrl: String = myBundle.sourceUrl
+            binding.wvInstructions.loadUrl(websiteUrl)
+        }
+
+        return binding.root
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
